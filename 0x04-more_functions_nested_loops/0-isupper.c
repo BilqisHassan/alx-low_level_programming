d61d6a27773bdc8dd71d46eae8c001d6e2770258
Bilqis Hassan <bilqieshassan@gmail.com>
2022-09-15 16:52:17 -0700

    c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -5,7 +5,13 @@
  * @c: the character to be checked
  * Return: 1 if c is uppercase, 0 otherwise
  */
+
 int _isupper(int c)
 {
-	return (c >= 'A' && c <= 'Z');
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+
+	return (0);
 }
